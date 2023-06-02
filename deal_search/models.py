27c605a6423b63435ab4d4dc7933e07cf71ba539
Michael Fraundorfer <1938084+Michi0301@users.noreply.github.com>
2023-06-02 21:38:28 +0200

    Adds search type.

--- deal_search/models.py
+++ deal_search/models.py
@@ -8,6 +8,7 @@ class SearchRequest(models.Model):
     provider = models.CharField(max_length=10)
     product_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    search_type = models.CharField(max_length=10)
 
 class Branch(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
